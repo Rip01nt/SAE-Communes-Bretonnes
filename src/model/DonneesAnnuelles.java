87f15ec9f150e7028ac82986221d3a0d9f0e5900
Rip01nt <beachsurferkid@gmail.com>
2024-05-23 15:36:49 +0200

    First push

---
+++ src/model/DonneesAnnuelles.java
@@ -0,0 +1,13 @@
+package model;
+
+public class DonneesAnnuelles {
+
+    private Annee lAnnee;
+    private Commune laCommune;
+    private int nbMaison;
+    private int nbAppart;
+    private double prixMoyen;
+    private double prixM2Moyen;
+
+
+}
