7b847f55a7daf5a89db149e6ef7db4aa8b9cc126
Rip01nt <beachsurferkid@gmail.com>
2024-05-23 15:56:34 +0200

    Update DonneesAnnuelles.java

--- src/model/DonneesAnnuelles.java
+++ src/model/DonneesAnnuelles.java
@@ -8,6 +8,107 @@ public class DonneesAnnuelles {
     private int nbAppart;
     private double prixMoyen;
     private double prixM2Moyen;
+    private double surfaceMoy;
+    private double depensesCultutrellesTotales;
+    private double budgetTotal;
+    private int population;
+
+    
+    public DonneesAnnuelles() {
+    }
+
+    public DonneesAnnuelles(Annee lAnnee, Commune laCommune, int nbMaison, int nbAppart, double prixMoyen, double prixM2Moyen, double surfaceMoy, double depensesCultutrellesTotales, double budgetTotal, int population) {
+        this.lAnnee = lAnnee;
+        this.laCommune = laCommune;
+        this.nbMaison = nbMaison;
+        this.nbAppart = nbAppart;
+        this.prixMoyen = prixMoyen;
+        this.prixM2Moyen = prixM2Moyen;
+        this.surfaceMoy = surfaceMoy;
+        this.depensesCultutrellesTotales = depensesCultutrellesTotales;
+        this.budgetTotal = budgetTotal;
+        this.population = population;
+    }
+
+    public Annee getLAnnee() {
+        return this.lAnnee;
+    }
+
+    public void setLAnnee(Annee lAnnee) {
+        this.lAnnee = lAnnee;
+    }
+
+    public Commune getLaCommune() {
+        return this.laCommune;
+    }
+
+    public void setLaCommune(Commune laCommune) {
+        this.laCommune = laCommune;
+    }
+
+    public int getNbMaison() {
+        return this.nbMaison;
+    }
+
+    public void setNbMaison(int nbMaison) {
+        this.nbMaison = nbMaison;
+    }
+
+    public int getNbAppart() {
+        return this.nbAppart;
+    }
+
+    public void setNbAppart(int nbAppart) {
+        this.nbAppart = nbAppart;
+    }
+
+    public double getPrixMoyen() {
+        return this.prixMoyen;
+    }
+
+    public void setPrixMoyen(double prixMoyen) {
+        this.prixMoyen = prixMoyen;
+    }
+
+    public double getPrixM2Moyen() {
+        return this.prixM2Moyen;
+    }
+
+    public void setPrixM2Moyen(double prixM2Moyen) {
+        this.prixM2Moyen = prixM2Moyen;
+    }
+
+    public double getSurfaceMoy() {
+        return this.surfaceMoy;
+    }
+
+    public void setSurfaceMoy(double surfaceMoy) {
+        this.surfaceMoy = surfaceMoy;
+    }
+
+    public double getDepensesCultutrellesTotales() {
+        return this.depensesCultutrellesTotales;
+    }
+
+    public void setDepensesCultutrellesTotales(double depensesCultutrellesTotales) {
+        this.depensesCultutrellesTotales = depensesCultutrellesTotales;
+    }
+
+    public double getBudgetTotal() {
+        return this.budgetTotal;
+    }
+
+    public void setBudgetTotal(double budgetTotal) {
+        this.budgetTotal = budgetTotal;
+    }
+
+    public int getPopulation() {
+        return this.population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
 
 
 }
