2ad8c35551cb57c24058827728474deccc3cdecc
Rip01nt <beachsurferkid@gmail.com>
2024-05-29 12:10:07 +0200

    Minor changes

--- src/view/communesView.java
+++ src/view/communesView.java
@@ -8,5 +8,20 @@ import javafx.stage.Stage;
 import controller.communesController;
 
 public class communesView extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/communes.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Communes Bretonnes");
+        primaryStage.show();
+        primaryStage.setResizable(false);
+        communesController controller = new communesController();
+        
+    }
     
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
--- ws/compile.sh
+++ ws/compile.sh
@@ -5,11 +5,4 @@ src_dir="../src"
 class_dir="../class"
 lib_dir="../lib"
 
-# Compile each Java file in the source directory
-for file in "$src_dir"/*.java; do
-    # Extract the file name without the extension
-    filename=$(basename "$file" .java)
-    
-    # Compile the file, specifying the classpath to include the lib directory
-    javac -cp "$lib_dir" -d "$class_dir" "$file"
-done
+javac -cp "$lib_dir" -d "$class_dir" $(find $src_dir -type f -name "*.java")
