58a63a8aab3e9bec08b10c0cdfd043ee81ae0b90
Rip01nt <beachsurferkid@gmail.com>
2024-05-31 10:36:49 +0200

    Update view (working)

--- src/controller/communesController.java
+++ src/controller/communesController.java
@@ -1,5 +1,27 @@
 package controller;
 
+import view.communesView;
+
 public class communesController {
 
+    private communesView view;
+
+    public communesController(communesView view) {
+        this.view = view;
+    }
+
+    public void onReady() {
+        view.getMenuButton().setOnAction(e -> {
+            view.getMenu().setVisible(!view.getMenu().isVisible());
+            if (!view.getMenu().isVisible()) {
+                view.getRoot().getChildren().remove(view.getMenu());
+            } else {
+                view.getRoot().setLeft(view.getMenu());
+            }
+        });
+        view.getExitButton().setOnAction(e -> {
+            view.exit();
+        });
+    }
+    
 }
