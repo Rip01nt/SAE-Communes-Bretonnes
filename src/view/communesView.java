3b06f4a1284e2bc70fe812843f1368be763fb435
Rip01nt <beachsurferkid@gmail.com>
2024-06-16 00:37:14 +0200

    Update communesView.java

--- src/view/communesView.java
+++ src/view/communesView.java
@@ -8,15 +8,14 @@ import javafx.geometry.Pos;
 //import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
+import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
@@ -35,8 +34,9 @@ public class communesView extends Application {
     private static final String TITLE = "Communes Bretonnes";
     private static final Label TITLE_LABEL = new Label(TITLE);
     private static final int IMG_SIZE = 80;
-    private static final Font TITLE_FONT = new Font("Arial", IMG_SIZE/2);
-    private static final Font FONT = new Font("Arial", IMG_SIZE/4);
+    private static final String FONT_NAME="Arial";
+    private static final Font TITLE_FONT = new Font(FONT_NAME, IMG_SIZE/2);
+    private static final Font FONT = new Font(FONT_NAME, IMG_SIZE/4);
     private static final Insets INSETS = new Insets(IMG_SIZE/10, IMG_SIZE/10, IMG_SIZE/10, IMG_SIZE/10);
     //private static final String FXML = "/ressource/communes.fxml";
     //private static final String CSS = "/ressource/communes.css";
@@ -112,6 +112,30 @@ public class communesView extends Application {
         BorderPane.setAlignment(menuButton, javafx.geometry.Pos.CENTER_LEFT);
 
         root.setCenter(this.rickRoll());
+
+        userButton.setOnAction(e -> {
+            root.setCenter(this.userView());
+        });
+
+        searchButton.setOnAction(e -> {
+            root.setCenter(this.searchView());
+        });
+
+        homeButton.setOnAction(e -> {
+            root.setCenter(this.homeView());
+        });
+
+        scoreboardButton.setOnAction(e -> {
+            root.setCenter(this.scoreboardView());
+        });
+
+        settingsButton.setOnAction(e -> {
+            root.setCenter(this.settingsView());
+        });
+        logo.setOnMouseClicked(e -> {
+            root.setCenter(this.rickRoll());
+        });
+
 
         this.center = new StackPane(root, this.authPane());
         this.scene = new Scene(center, 720, 740);
@@ -234,5 +258,157 @@ public class communesView extends Application {
         authPane.setAlignment(Pos.CENTER);
 
         return authPane;
+    }
+
+    private GridPane userView() {
+        GridPane userPane = new GridPane();
+
+        return userPane;
+    }
+
+
+    private GridPane homeView() {
+        GridPane homePane = new GridPane();
+
+
+        return homePane;
+    }
+
+    private GridPane searchView() {
+        GridPane searchPane = new GridPane();
+
+        return searchPane;
+    }
+
+    private GridPane scoreboardView() {
+        GridPane scoreboardPane = new GridPane();
+
+        return scoreboardPane;
+    }
+
+    private GridPane settingsView() {
+        GridPane dataPane = new GridPane();
+
+        Label labelCommunesVoisines = new Label("Nombre de communes voisines");
+
+        Slider sliderCommunesVoisines = new Slider();
+        sliderCommunesVoisines.setShowTickMarks(true);
+        sliderCommunesVoisines.setShowTickLabels(true);
+        sliderCommunesVoisines.setScaleShape(false);
+        sliderCommunesVoisines.setMajorTickUnit(10);
+        sliderCommunesVoisines.setBlockIncrement(1);
+
+        Label labelVenteApart = new Label("Nombre d'appartements vendu par ans");
+
+        Slider sliderVenteApart = new Slider();
+        sliderVenteApart.setShowTickMarks(true);
+        sliderVenteApart.setShowTickLabels(true);
+        sliderCommunesVoisines.setScaleShape(true);
+        sliderVenteApart.setMajorTickUnit(10);
+        sliderVenteApart.setBlockIncrement(1);
+
+        Label labelTailleMoyHabit = new Label("Taille moyenne des habitations");
+
+        Slider sliderTailleMoyHabit = new Slider();
+        sliderTailleMoyHabit.setShowTickMarks(true);
+        sliderTailleMoyHabit.setShowTickLabels(true);
+        sliderTailleMoyHabit.setMajorTickUnit(10);
+        sliderTailleMoyHabit.setBlockIncrement(1);
+
+        Label labelInflation = new Label("Taux inflation");
+
+        Slider sliderInflation = new Slider();
+        sliderInflation.setShowTickMarks(true);
+        sliderInflation.setShowTickLabels(true);
+        sliderInflation.setMajorTickUnit(10);
+        sliderInflation.setBlockIncrement(1);
+
+        Label labelInvCulture = new Label("Investissement culturel");
+
+        Slider sliderInvCulture = new Slider();
+        sliderInvCulture.setShowTickMarks(true);
+        sliderInvCulture.setShowTickLabels(true);
+        sliderInvCulture.setMajorTickUnit(10);
+        sliderInvCulture.setBlockIncrement(1);
+
+        Label labelPrixMoyM2Ans = new Label("Prix moyen du m² par ans");
+
+        Slider sliderPrixMoyM2Ans = new Slider();
+        sliderPrixMoyM2Ans.setShowTickMarks(true);
+        sliderPrixMoyM2Ans.setShowTickLabels(true);
+        sliderPrixMoyM2Ans.setMajorTickUnit(10);
+        sliderPrixMoyM2Ans.setBlockIncrement(1);
+
+        Label labelNbGare = new Label("Nombre de Gare");
+
+        Slider sliderNbGare = new Slider();
+        sliderNbGare.setShowTickMarks(true);
+        sliderNbGare.setShowTickLabels(true);
+        sliderNbGare.setMajorTickUnit(10);
+        sliderNbGare.setBlockIncrement(1);
+
+        Label labelNbHabitant = new Label("Nombre d'habitant");
+
+        Slider sliderNbHabitant = new Slider();
+        sliderNbHabitant.setShowTickMarks(true);
+        sliderNbHabitant.setShowTickLabels(true);
+        sliderNbHabitant.setMajorTickUnit(10);
+        sliderNbHabitant.setBlockIncrement(1);
+
+        Label labelNbMaisonVenduAns = new Label("Nombre de maison vendu par ans");
+
+        Slider sliderNbMaisonVenduAns = new Slider();
+        sliderNbMaisonVenduAns.setShowTickMarks(true);
+        sliderNbMaisonVenduAns.setShowTickLabels(true);
+        sliderNbMaisonVenduAns.setMajorTickUnit(10);
+        sliderNbMaisonVenduAns.setBlockIncrement(1);
+
+        Label labelNbAeroport = new Label("Nombre d\'aéroport");
+
+        Slider sliderNbAeroport = new Slider();
+        sliderNbAeroport.setShowTickMarks(true);
+        sliderNbAeroport.setShowTickLabels(true);
+        sliderNbAeroport.setMajorTickUnit(10);
+        sliderNbAeroport.setBlockIncrement(1);
+
+        Label[] labels = new Label[]{labelCommunesVoisines, labelVenteApart, labelTailleMoyHabit, labelInflation, labelInvCulture, labelPrixMoyM2Ans, labelNbGare, labelNbHabitant, labelNbMaisonVenduAns, labelNbAeroport};
+
+        for (int i = 0; i < labels.length; i++) {
+            labels[i].setFont(new Font(FONT_NAME, IMG_SIZE/5));
+            GridPane.setMargin(labels[i], new Insets(10));
+            GridPane.setHalignment(labels[i], HPos.CENTER);
+        }
+
+        dataPane.add(labelCommunesVoisines, 0, 0);
+        dataPane.add(sliderCommunesVoisines, 0, 1);
+
+        dataPane.add(labelVenteApart, 1, 0);
+        dataPane.add(sliderVenteApart, 1, 1);
+
+        dataPane.add(labelTailleMoyHabit, 0, 2);
+        dataPane.add(sliderTailleMoyHabit, 0, 3);
+
+        dataPane.add(labelInflation, 1, 2);
+        dataPane.add(sliderInflation, 1, 3);
+
+        dataPane.add(labelInvCulture, 0, 4);
+        dataPane.add(sliderInvCulture, 0, 5);
+
+        dataPane.add(labelPrixMoyM2Ans, 1, 4);
+        dataPane.add(sliderPrixMoyM2Ans, 1, 5);
+
+        dataPane.add(labelNbGare, 0, 6);
+        dataPane.add(sliderNbGare, 0, 7);
+
+        dataPane.add(labelNbHabitant, 1, 6);
+        dataPane.add(sliderNbHabitant, 1, 7);
+
+        dataPane.add(labelNbMaisonVenduAns, 0, 8);
+        dataPane.add(sliderNbMaisonVenduAns, 0, 9);
+
+        dataPane.add(labelNbAeroport, 1, 8);
+        dataPane.add(sliderNbAeroport, 1, 9);
+
+        return dataPane;
     }
 }
