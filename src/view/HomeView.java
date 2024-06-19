42ffe928c29b92f64827175d3459fdd014bbb82f
SevenMest <127766238+SevenMest@users.noreply.github.com>
2024-06-19 18:22:42 +0200

    Update HomeView.java

--- src/view/HomeView.java
+++ src/view/HomeView.java
@@ -4,13 +4,12 @@ import java.io.FileInputStream;
 
 import controller.communesController;
 import javafx.geometry.Insets;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+
 
 public class HomeView {
 
