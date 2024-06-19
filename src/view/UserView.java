fb8dfd13acdef27e6ec50253f840f8bc4f979f54
Rip01nt <beachsurferkid@gmail.com>
2024-06-19 19:00:19 +0200

    Update UserView.java

--- src/view/UserView.java
+++ src/view/UserView.java
@@ -5,6 +5,7 @@ import javafx.geometry.HPos;
 import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 
 public class UserView {
@@ -12,8 +13,6 @@ public class UserView {
     private GridPane userPane;
     private Label labelUsername;
     private Label labelPassword;
-    private Label labelUISize;
-    private Slider sliderUISize;
 
     public UserView(communesController controller) {
         userPane = new GridPane();
@@ -28,41 +27,20 @@ public class UserView {
         GridPane.setMargin(labelPassword, new Insets(10));
         GridPane.setHalignment(labelPassword, HPos.CENTER);
 
-        labelUISize = new Label("UI Size");
-        labelUISize.setFont(controller.getFONT());
-        GridPane.setMargin(labelUISize, new Insets(10));
-        GridPane.setHalignment(labelUISize, HPos.CENTER);
-        sliderUISize = new Slider();
-        sliderUISize.setShowTickMarks(true);
-        sliderUISize.setScaleShape(false);
-        sliderUISize.setMajorTickUnit(20);
-        sliderUISize.setBlockIncrement(20);
-        sliderUISize.setMin(60);
-        sliderUISize.setMax(100);
-
         userPane.add(labelUsername, 0, 0);
         userPane.add(labelPassword, 0, 1);
-        userPane.add(labelUISize, 0, 2);
-        userPane.add(sliderUISize, 1, 2);
     }
 
     public GridPane getPane() {
         return userPane;
     }
 
-    public Label getLabelUsername() {
+    public TextField getLabelUsername() {
         return labelUsername;
     }
 
-    public Label getLabelPassword() {
+    public TextField getLabelPassword() {
         return labelPassword;
     }
 
-    public Label getLabelUISize() {
-        return labelUISize;
-    }
-
-    public Slider getSliderUISize() {
-        return sliderUISize;
-    }
 }
