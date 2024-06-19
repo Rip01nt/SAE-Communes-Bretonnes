bd8902a87986fb0caacb89c54410a7bb5714a211
TheL9 <105281626+Elluan@users.noreply.github.com>
2024-06-19 20:20:59 +0200

    Update ScenarioDAO.java

--- src/test/model/ScenarioDAO.java
+++ src/test/model/ScenarioDAO.java
@@ -1,6 +1,10 @@
 package test.model;
-import model.dao.DAO;
+import model.dao.*;
 
 public class ScenarioDAO {
 
+    public static void main(String[] args){
+        CommuneDAO communeDAO = new CommuneDAO();
+        System.out.println(communeDAO.findByName("Vannes"));
+    }
 }
